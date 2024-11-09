f76ca32a5e7d8a435ce0859be434eafe4543d6fb
reepicheep213 <44447450+reepicheep213@users.noreply.github.com>
2024-11-08 18:21:25 -0800

    implement a progressively darkening effect

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const gridContainer = document.querySelector("#gridContainer");
 
-function randomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
+const btnContainer = document.createElement("div");
+btnContainer.classList.add("btnContainer");
+document.body.insertBefore(btnContainer, gridContainer);
+
+const startBtn = document.createElement("button");
+startBtn.textContent = "Start";
+btnContainer.appendChild(startBtn);
 
 function randomRgbColor() {
-  let r = randomInt(255);
-  let g = randomInt(255);
-  let b = randomInt(255);
-  return `rgb(${r}, ${g}, ${b})`;
+  return `rgb(${Math.floor(Math.random() * 256)}, 
+    ${Math.floor(Math.random() * 256)},
+    ${Math.floor(Math.random() * 256)})`;
 } 
 
 for (let i = 0; i < 16 ** 2; i++) {
   const initialGrid = document.createElement("div");
-  initialGrid.id = "initialGrid";
+  initialGrid.classList.add("initialGrid");
   gridContainer.appendChild(initialGrid);
 
   initialGrid.addEventListener("mouseover", () => {
@@ -21,42 +24,31 @@ for (let i = 0; i < 16 ** 2; i++) {
   });
 }
 
-const btnDiv = document.createElement("div");
-document.body.insertBefore(btnDiv, gridContainer);
+startBtn.addEventListener("click", () => {
+    let gridNum = prompt("How many squares per side do you want for your grid?");
+    if (gridNum === null) {
+      alert("Canceled!");
+    } else if (gridNum < 16 || gridNum > 100) {
+      alert("Too big!");
+    } else if (gridNum < 16) {
+      alert("Too small!");
+    } else {
+      startBtn.textContent = "Restart";
+      gridContainer.innerHTML = "";
 
-const button = document.createElement("button");
-button.textContent = "Start";
-btnDiv.appendChild(button);
-
-let breakElement = document.createElement("br");
-document.body.insertBefore(breakElement, gridContainer);
-
-button.addEventListener("click", () => {
-  let gridNum = prompt("How many squares per side do you want for your grid?");
-  if (gridNum === null) {
-    alert("Canceled!");
-  } else if (gridNum > 100) {
-    alert("Too big!");
-  } else if (gridNum < 16) {
-    alert("Too small!");
-  } else {
-    button.textContent = "Restart";
-    while (gridContainer.firstChild) {
-      gridContainer.removeChild(gridContainer.firstChild);
-    }
-
-    const gridDiv = window.getComputedStyle(gridContainer);
-    const squareSize = parseFloat(gridDiv.width) / gridNum;
+      const gridDiv = window.getComputedStyle(gridContainer);
+      const squareSize = parseFloat(gridDiv.width) / gridNum;
 
     for (let i = 0; i < gridNum ** 2; i++) {
       const newGrid = document.createElement("div");
       newGrid.style.width = `${squareSize}px`;
       newGrid.style.height = `${squareSize}px`;
-      
       gridContainer.appendChild(newGrid);
       newGrid.addEventListener("mouseover", () => {
-        newGrid.style.backgroundColor = randomRgbColor();
+        newGrid.style.backgroundColor = "black";
+        let opacity = parseFloat(newGrid.style.opacity) || 0.1;
+        newGrid.style.opacity = Math.min(opacity + 0.1, 1);
       });
-    }
+    };
   }
-})
+});
